aa26add710372c97511bb4519b786c19ab2c5fe4
furimeo <lehungquangminh2011@gmail.com>
2026-09-03 22:27:46 +0700

    chore: initial Spring Boot skeleton
    Spring Boot 4.1.x project with the GitKoo dependency set:
    Spring Web MVC, Security, Data JDBC, Thymeleaf, Validation,
    SQLite driver. Gradle Groovy DSL, Java 25 toolchain.

---
+++ src/main/java/com/furimeo/gitkoo/GitkooApplication.java
@@ -0,0 +1,13 @@
+package com.furimeo.gitkoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitkooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitkooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furimeo/gitkoo/GitkooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furimeo.gitkoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitkooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
